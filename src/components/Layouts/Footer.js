c425dcfd1d882067bad0c69b56c9529b7cd0b5d4
Famakinwa Temitope <famakinwatemitope@gmail.com>
2025-03-04 12:11:38 +0100

    updated

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,18 +1,16 @@
-export default function  Footer() {
-    return (
-        <footer className="footer">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-sm-6">
-                        2024 © JustFood.
-                    </div>
-                    <div className="col-sm-6">
-                        <div className="text-sm-end d-none d-sm-block">
-                            Design & Develop by DIGITALWEB
-                        </div>
-                    </div>
-                </div>
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-sm-6">2024 © Organisation Repo.</div>
+          <div className="col-sm-6">
+            <div className="text-sm-end d-none d-sm-block">
+              Design & Develop by Temitope
             </div>
-        </footer>
-    )
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
